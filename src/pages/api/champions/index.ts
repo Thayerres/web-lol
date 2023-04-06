a21f6df2c0facdf6bb1abc209a15b17729eebcaa
Thayerres <thayerres@gmail.com>
2023-04-06 14:04:39 -0300

    feat: create type champion

--- src/pages/api/champions/index.ts
+++ src/pages/api/champions/index.ts
@@ -1,40 +1,36 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { getDataJson } from '@/shared/getDataJson'
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-
-type Champions = {
-  id: string,
-  key: string,
-  name: string
-}
+import {getDataJson} from '@/shared/getDataJson';
+import {IChampionTiles} from '@/types/championTiles';
+import type {NextApiRequest, NextApiResponse} from 'next';
 
 type Data = {
-  champions: Champions[],
-  version: string
-}
+  champions: IChampionTiles[];
+  version: string;
+};
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
   if (req.method !== 'GET') {
-    return res.status(405).end()
+    return res.status(405).end();
   }
- 
-  const data = await getDataJson()
- 
-  const champions = Object.values(data.data).map((value:any):Champions => {
-    return {
-      id: value.id,
-      key: value.key,
-      name: value.name
+
+  const data = await getDataJson();
+
+  const champions = Object.values(data.data).map(
+    (value: any): IChampionTiles => {
+      return {
+        id: value.id,
+        key: value.key,
+        name: value.name,
+      };
     }
-  })
-  const version: string = data.version
+  );
+  const version: string = data.version;
 
   return res.status(200).json({
     version,
-    champions
+    champions,
   });
 }
---
+++ src/types/championTiles.ts
@@ -0,0 +1,5 @@
+export type IChampionTiles = {
+  id: string;
+  key: string;
+  name: string;
+};
